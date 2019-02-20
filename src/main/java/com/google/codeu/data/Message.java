7a73c5f1eb3ac26f6e2a743689427f27dee43906
Emily Tran <emilytran@etran.local>
2019-02-20 01:45:06 -0500

    added recipient field for messages to java files

--- src/main/java/com/google/codeu/data/Message.java
+++ src/main/java/com/google/codeu/data/Message.java
@@ -25,6 +25,7 @@ public class Message {
   private String user;
   private String text;
   private long timestamp;
+  private String recipient;
 
   /**
    * Constructs a new {@link Message} posted by {@code user} with {@code text} content. Generates a
@@ -39,6 +40,18 @@ public class Message {
     this.user = user;
     this.text = text;
     this.timestamp = timestamp;
+  }
+
+  public Message(String user, String text, String recipient) {
+    this(UUID.randomUUID(), user, text, System.currentTimeMillis(), recipient);
+  }
+
+  public Message(UUID id, String user, String text, long timestamp, String recipient) {
+    this.id = id;
+    this.user = user;
+    this.text = text;
+    this.timestamp = timestamp;
+    this.recipient = recipient;
   }
 
   public UUID getId() {
@@ -56,4 +69,8 @@ public class Message {
   public long getTimestamp() {
     return timestamp;
   }
+
+  public String getRecipient(){
+  return recipient;
+}
 }
