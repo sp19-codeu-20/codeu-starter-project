0318538b6463976e52d749e48a09a26331a596bc
Emily Tran <32020652+emilytran99@users.noreply.github.com>
2019-02-20 19:42:59 -0500

    Updated indentation, removed extraneous constructors
    Corrected indentation for getRecipient function and removed extraneous constructors

--- src/main/java/com/google/codeu/data/Message.java
+++ src/main/java/com/google/codeu/data/Message.java
@@ -31,21 +31,10 @@ public class Message {
    * Constructs a new {@link Message} posted by {@code user} with {@code text} content. Generates a
    * random ID and uses the current system time for the creation time.
    */
-  public Message(String user, String text) {
-    this(UUID.randomUUID(), user, text, System.currentTimeMillis());
-  }
-
-  public Message(UUID id, String user, String text, long timestamp) {
-    this.id = id;
-    this.user = user;
-    this.text = text;
-    this.timestamp = timestamp;
-  }
-
   public Message(String user, String text, String recipient) {
     this(UUID.randomUUID(), user, text, System.currentTimeMillis(), recipient);
   }
-
+  
   public Message(UUID id, String user, String text, long timestamp, String recipient) {
     this.id = id;
     this.user = user;
@@ -71,6 +60,6 @@ public class Message {
   }
 
   public String getRecipient(){
-  return recipient;
-}
+    return recipient;
+  }
 }
