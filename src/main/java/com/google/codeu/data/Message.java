eeab83a7cfdd4c1cd910ace459c4cfcc4d5a1163
jover <jover@mit.edu>
2019-03-30 18:48:00 -0400

    For stickler

--- src/main/java/com/google/codeu/data/Message.java
+++ src/main/java/com/google/codeu/data/Message.java
@@ -39,7 +39,8 @@ public class Message {
   /**
    * Message Parameters.
    */
-  public Message(UUID id, String user, String text, long timestamp, String recipient, String imageUrl) {
+  public Message(UUID id, String user, String text, 
+  long timestamp, String recipient, String imageUrl) {
     this.id = id;
     this.user = user;
     this.text = text;
